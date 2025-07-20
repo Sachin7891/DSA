145980f2f0bbdc94418127fe2392f1753e1801a4
sachin7891 <guptasachin7898@gmail.com>
2025-07-21 01:51:12 +0530

    to find factorial of any no.

---
+++ DSAwithJAVA/factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int res = 1;
+        while (n>0) {
+            res = res*n;
+            n--;
+            
+        }
+        System.out.println(res);
+    }
+}
